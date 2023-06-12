fe87410494e4a8be8814b762b88181544770ca78
Jose Ricardo Peixoto <peixoto.service@hotmail.com>
2023-06-12 20:37:06 -0300

    estrutura  basica
    upload estrutura  básica sem refinamento.

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<meta charset="UTF-8" lang="pt-br">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+    <title>SPGWeb</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.min.js">
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.esm.min.js">
+    
+</head>
+<body>
+    <iframe>
+        
+    </iframe>
+        <header class="d-flex">
+            <div>
+                logo
+            </div>
+            <nav>
+                menu            
+            </nav>
+        </header>
+        <main class="d-flex">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellat officiis at tempora nam voluptates. Quas sint, deserunt quisquam doloribus expedita veniam enim velit beatae quaerat exercitationem, laudantium vel temporibus!
+        </main>
+        <footer>
+                rodapé
+        </footer>
+        </header>
+</body>
+</html>
